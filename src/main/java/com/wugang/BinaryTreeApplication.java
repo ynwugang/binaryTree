569f67af21b739b2cfb0c0872f117b4640a1570b
ynwugang <996528491@qq.com>
2022-06-16 15:59:43 +0800

    增加logback配置文件

--- src/main/java/com/wugang/BinaryTreeApplication.java
+++ src/main/java/com/wugang/BinaryTreeApplication.java
@@ -1,13 +1,22 @@
 package com.wugang;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 @SpringBootApplication
 public class BinaryTreeApplication {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(BinaryTreeApplication.class);
+
     public static void main(String[] args) {
-        SpringApplication.run(BinaryTreeApplication.class, args);
+        ConfigurableEnvironment environment = SpringApplication.run(BinaryTreeApplication.class, args).getEnvironment();
+
+        LOGGER.info("启动成功！");
+        LOGGER.info("地址:\thttp://localhost:{}",environment.getProperty("server.port"));
     }
 
 }

14ae3f0fc7287125354fe9112c4f3e8a6bf6844f
ynwugang <996528491@qq.com>
2022-06-23 22:23:58 +0800

    集成elementUI

--- web/src/main.ts
+++ web/src/main.ts
@@ -3,4 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+// element plus
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
+const app = createApp(App)
+
+app.use(store).use(router).use(ElementPlus)
+app.mount('#app')

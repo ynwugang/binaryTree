3b8464dece3842fab4000be4d24512b43bebeb90
ynwugang <996528491@qq.com>
2022-07-13 14:53:13 +0800

    配置axios拦截器，打印请求和返回参数

--- web/src/main.ts
+++ web/src/main.ts
@@ -16,6 +16,23 @@ import axios from "axios";
 //修改axios的baseURL为配置文件中定义的服务端地址
 axios.defaults.baseURL = process.env.VUE_APP_SERVER;
 
+/**
+ * axios拦截器
+ */
+axios.interceptors.request.use(function (config){
+    console.log('请求参数：', config);
+    return config;
+}, error => {
+    return Promise.reject(error);
+});
+axios.interceptors.response.use(function (response){
+    console.log('返回结果：', response);
+    return response;
+}, error => {
+    console.log('返回错误：', error);
+    return Promise.reject(error);
+});
+
 const app = createApp(App)
 
 app.use(store).use(router).use(ElementPlus)
--- web/src/views/HomeView.vue
+++ web/src/views/HomeView.vue
@@ -182,7 +182,6 @@ export default defineComponent({
           .then((response) => {
             const data = response.data;
             ebooks.value = data.content;
-            console.log(response);
           });
     });
 
